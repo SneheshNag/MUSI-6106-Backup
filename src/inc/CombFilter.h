2bb610525632478297cf5dbcb928dfe4e9e77a8e
Snehesh Nag <33204313+SneheshNag@users.noreply.github.com>
2020-01-22 17:17:20 +0530

    Update CombFilter.h

--- src/inc/CombFilter.h
+++ src/inc/CombFilter.h
@@ -1,1 +1,29 @@
+#ifndef FilterAudio_hpp
+#define FilterAudio_hpp
 
+#include <stdio.h>
+
+
+class FilterAudio {
+public:
+    FilterAudio(float fFIRCoeff, float fIIRCoeff, int iDelayInSamples, int iNumChannels); // Constructor
+    ~FilterAudio();
+    
+    float **combFilterBlock(float **input, int blockSize, int numChannels);
+
+    void clearDelayLines();
+    
+    int getDelayInSamples() const;
+    float getFIRCoeff() const;
+    float getIIRCoeff() const;
+
+private:
+    float fFIRCoeff;
+    float fIIRCoeff;
+    int iDelayInSamples;
+    int iNumChannels;
+    float **fFIRDelay;
+    float **fIIRDelay;
+};
+
+#endif /* FilterAudio_h */
